6cc816577590e45e5a8c1133600d7eb460cc632a
Guzel Karabaeva <karabaeva.guzel@yandex.ru>
2023-10-06 14:09:32 +0300

    New commit

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,10 +5,8 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    Feline feline;
 
-    public Lion(Feline feline, String sex) throws Exception {
-        this.feline = feline;
+    public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -17,6 +15,8 @@ public class Lion {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
+
+    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
